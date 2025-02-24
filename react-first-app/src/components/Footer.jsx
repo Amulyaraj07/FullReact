2549ce3f640146c00be19394c608e76ddc01f128
amulya <amulyaraj1234@gmail.com>
2025-02-25 02:01:41 +0530

    Added new files

---
+++ react-first-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>copyright@MyWebsite 2025</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ react-first-app/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h2>Main content</h2>
+      <p>This is the main Component</p>
+    </div>
+  )
+}
+
+export default Main
